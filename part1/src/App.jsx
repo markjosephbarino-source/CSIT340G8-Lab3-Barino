f849a7126ca4583c94b3a76c46648694930dd300
Mark Joseph Barino <markjoseph.barino@gmail.com>
2026-09-21 20:50:18 +0800

    1.1: Course Information step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,26 @@
-import './App.css'
-
-function App() {
+const App = () => {
+  const course = 'CSIT340'
+  const part1 = 'Introduction to React'
+  const exercises1 = 3
+  const part2 = 'DOM Tree'
+  const exercises2 = 4
+  const part3 = 'Output Tracing'
+  const exercises3 = 10
 
   return (
-    
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
   )
 }
 
